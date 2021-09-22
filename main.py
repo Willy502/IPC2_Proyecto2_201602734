c95779b796464a05071a89858e192af3e3a4ac32
Alejandro Barrios <w.alejandro.barrios@gmail.com>
2021-09-21 22:47:46 -0600

    Start with GUI

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.gui.gui import *
+
+class Main:
+
+    def __init__(self):
+        Gui()
+
+Main()
---
+++ src/gui/gui.py
@@ -0,0 +1,7 @@
+class Gui:
+
+    def __init__(self):
+        self.build_gui()
+
+    def build_gui(self):
+        print("BUILD GUI AJUA")
