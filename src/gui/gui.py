36636fa11f1c88b547dd7c1fc5b9eec96b05daf8
Alejandro Barrios <w.alejandro.barrios@gmail.com>
2021-09-24 00:58:08 -0600

    Load file

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
-
+from tkinter import filedialog as fd
+from tkinter import messagebox
 
 class Gui:
 
@@ -10,23 +11,24 @@ class Gui:
     def build_gui(self):
         self.root = tk.Tk()
         self.root.title("Línea de ensamblaje")
-        tabControl = ttk.Notebook(self.root, width = 500, height = 500)
+        self.tabControl = ttk.Notebook(self.root, width = 500, height = 500)
   
-        tab1 = ttk.Frame(tabControl)
-        tab2 = ttk.Frame(tabControl)
-        tab3 = ttk.Frame(tabControl)
+        tab1 = ttk.Frame(self.tabControl)
+        tab2 = ttk.Frame(self.tabControl)
+        tab3 = ttk.Frame(self.tabControl)
         
-        tabControl.add(tab1, text ='Cargar Archivos')
-        tabControl.add(tab2, text ='Ejecutar ensamblaje')
-        tabControl.add(tab3, text ='Ayuda')
-        tabControl.pack(expand = True, fill = tk.BOTH)
-        
-        ttk.Label(tab1, 
-                text ="Welcome to \
-                GeeksForGeeks").grid(column = 0, 
-                                    row = 0,
-                                    padx = 30,
-                                    pady = 30)  
+        self.tabControl.add(tab1, text ='Cargar Archivos')
+        self.tabControl.add(tab2, text ='Ejecutar ensamblaje')
+        self.tabControl.add(tab3, text ='Ayuda')
+        self.tabControl.pack(expand = True, fill = tk.BOTH)
+
+        entry_machine = ttk.Entry(tab1, state = tk.DISABLED)
+        entry_machine.grid(column = 0, row = 0)
+        button_machine = ttk.Button(tab1, text = "Cargar máquina", command = lambda:self.on_click(option_clicked=1, entry=entry_machine)).grid(column = 1, row = 0)
+        entry_simulation = ttk.Entry(tab1, state = tk.DISABLED)
+        entry_simulation.grid(column = 0, row = 1)
+        button_simulation = ttk.Button(tab1, text = "Cargar simulación", command = lambda:self.on_click(option_clicked=2, entry=entry_simulation)).grid(column = 1, row = 1)
+
         ttk.Label(tab2,
                 text ="Lets dive into the\
                 world of computers").grid(column = 0,
@@ -34,8 +36,9 @@ class Gui:
                                             padx = 30,
                                             pady = 30)
 
-        ttk.Label(tab3, text =  self.information()).grid(column = 0,
-                                            row = 0)
+        ttk.Label(tab3, text =  self.information()).grid(column = 0, row = 0)
+
+        self.root.bind("<Button>", self.on_change_tab)
         self.root.mainloop()
 
     def information(self):
@@ -44,3 +47,29 @@ class Gui:
         info += "Introducción a la programación y computación 2 \n"
         info += "Sección  \n"
         return info
+
+    def on_click(self, option_clicked, entry):
+
+        file = fd.askopenfilename(title='Open file', filetypes=[('text files', '*.xml')])
+
+        if file != "":
+
+            entry.configure(state=tk.NORMAL)
+            entry.delete(0, tk.END)
+            entry.insert(0, file)
+            entry.configure(state=tk.DISABLED)
+
+            if option_clicked == 1:
+                print("OPTION 1")
+            elif option_clicked == 2:
+                print("OPTION 2")
+
+        else:
+            messagebox.showinfo("Error", "No se ha cargado ningún archivo")
+
+    def on_change_tab(self, event):
+        name = self.tabControl.select()
+        index = self.tabControl.index(name)
+        if index == 1:
+            self.tabControl.select(0)
+            messagebox.showinfo("Error", "No has cargado la máquina o el archivo de simulación")
